9895ab8eae7f2914c946a331b82fba26cab7a2c6
oliver <oshmusic11@gmail.com>
2025-07-05 18:01:21 -0400

    Added some class names and a way to reference Stat components by id in CSS to Stat.jsx

--- src/Stat.jsx
+++ src/Stat.jsx
@@ -26,11 +26,11 @@ const Stat = ( { title, value, isNumeric, id, removeStat } ) => {
     // is that actually necessary? it seems to be saving the values on its own
     // if I want this to persist on page reload (cookies) then I would need to do this
     return (
-        <div style={{border: 'black solid 2px'}}>
+        <div style={{border: 'black solid 2px'}} id={'stat-'+ id} className='statWidget'>
             <div>
                 <button onClick={() => removeStat(id)}>X</button>
             </div>
-            <div>
+            <div className='typeSelectForm'>
                 <form onSubmit={(event) => handleSubmit(event)}> 
                     <input type="radio" id="numeric" name="typeSelect"></input>
                     <label for="numeric">Numeric</label><br />
@@ -38,16 +38,16 @@ const Stat = ( { title, value, isNumeric, id, removeStat } ) => {
                     <label for="text">Text</label> <br />
                 </form>
             </div>
-            <div>
+            <div className='widgetTitle'>
                 <input type="text" placeholder={"Title will go here"}></input>
             </div>
-            <div>
+            <div className='widgetNumbers'>
                 { /* Remove default buttons with CSS */}
                 <button onClick={() => incAndDec(false)}>-</button>
                 <input type="number" placeholder={"Number will go here"} ref={numberVal} id={'number-' + id}></input>
                 <button onClick={() => incAndDec(true)}>+</button>
             </div>
-            <div>
+            <div className='widgetText'>
                 <input type="text" placeholder={"Text will go here"}></input>
             </div>
         </div>
